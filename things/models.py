172b8db7d5beffd6096ddbb6fe6bdfaab36a12f2
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-10-07 16:16:21 +0100

    Adhere to testing criteria

--- things/models.py
+++ things/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MaxLengthValidator
 # Create your models here.
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True, max_length=120) 
-    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100),])
+    description = models.TextField(blank=True, validators=[MaxLengthValidator(120)],) 
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100),])
